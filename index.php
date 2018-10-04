fb2fafcb540113076852532c886cfb25044da7a7
Valdir <valdir.nat@live.com>
2018-10-04 20:37:41 -0300

    Criamos a classe ususario

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 
 require_once("config.php");
 
-$sql= new Sql();
+/*$sql= new Sql();
 
 $usuarios=$sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
+
+
+$root = new Usuario();
+
+$root->loadById(1);
+
+echo $root;
+
+
  ?>
