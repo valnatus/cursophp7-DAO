16efcdf40444c254f8ca0fd5a02e1fc6e05dbdd4
Valdir <valdir.nat@live.com>
2018-10-18 19:39:41 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -8,12 +8,23 @@ $usuarios=$sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
-
+/*
 $root = new Usuario();
 
 $root->loadById(1);
 
 echo $root;
+*/
 
+//$lista = Usuario::getList();
 
+//echo json_encode($lista);
+
+//$search=Usuario::search("jo");
+//echo json_encode($search);
+
+$usuario = new Usuario();
+$usuario->login("root","1234");
+
+echo $usuario;
  ?>
